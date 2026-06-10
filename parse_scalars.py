0d16dbf3c6c4da5b2872072f69f3ab3d6109bdc2
Sam Hilliard <samhilliard51@gmail.com>
2026-06-10 10:48:09 -0400

    Parse custom scalars output to file

--- parse_scalars.py
+++ parse_scalars.py
@@ -13,7 +13,7 @@ def get_custom_scalars(introspection_resp):
         if isinstance(type_obj, GraphQLScalarType) and not is_specified_scalar_type(type_obj)
     ]
 
-    print(custom_scalars)
+    return custom_scalars
 
 def load_file():
 
@@ -37,10 +37,22 @@ def load_file():
         print(f"Error: '{file_path}' is not a valid JSON file.")
         sys.exit(1)
 
+def export_json(scalars_list):
+    scalars_dict = {scalar: None for scalar in scalars_list}
+
+    output_filename = "custom_scalars.json"
+
+    with open(output_filename, "w", encoding="utf-8") as file:
+        json.dump(scalars_dict, file, indent=4)
+
+    print(f"Custom scalars saved to {output_filename}")
+    print(f"Fill in the `null` values for the most accurate results")
+
 
 def main():
     introspection_data = load_file()
-    get_custom_scalars(introspection_data)
+    custom_scalars = get_custom_scalars(introspection_data)
+    export_json(custom_scalars)
 
 if __name__ == '__main__':
     main()
